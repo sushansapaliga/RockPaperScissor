88e253642afa4b32ce669d5d21982c834f5e8862
Sushan Sapaliga <sushansapaliga@gmail.com>
2020-07-25 01:32:52 +0530

    3rd phase

--- public/js/playWithBot.js
+++ public/js/playWithBot.js
@@ -10,6 +10,11 @@ const actionModel = document.querySelector(".actionModel");
 const actionModelHeader = document.querySelector(".actionModelHeader");
 const actionModelBody = document.querySelector(".actionModelBody");
 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorBtn = document.querySelector("#scissor");
+const moveCollections = document.querySelector(".moveCollections");
+
 
 // menu drawer
 menuBtnOpen.addEventListener("click",()=>{
@@ -108,6 +113,107 @@ function flyerModel(message, status){
     },3000);
 }
 
+// return the winner
+function checkTheWinner(player1, player2) {
+    
+    if(player1 == player2){
+        return 'draw';
+    }else if((player1 == "Rock" && player2 == "Paper") || (player1 == "Paper" && player2 == "Scissor") || (player1 == "Scissor" && player2 == "Rock")){
+        return '2';
+    }else{
+        return '1';
+    }
+}
+
+// find the move from the robot
+function moveFromRobot(){
+
+    let move = Math.random()*100;
+
+    if(move > 66){
+        return 'Rock';
+    }else if(move > 33){
+        return 'Paper';
+    }else{
+        return 'Scissor';
+    }
+
+}
+
+// fires when user selects an option
+function userChooseMove(userMove){
+
+    moveCollections.classList.add("optionDisabled");
+    moveCollections.classList.remove("userOptions");
+
+    // event removal - disabling the button 
+    rockBtn.removeEventListener("click",functionForRock);
+    paperBtn.removeEventListener("click",functionForPaper);
+    scissorBtn.removeEventListener("click",functionForScissor);
+
+    const selectedMove = document.querySelector(".selectedMove");
+    const descriptionFromRobot = document.querySelector(".descriptionFromRobot");
+
+    selectedMove.innerHTML = userMove;
+    descriptionFromRobot.innerHTML = "Robot Is Thinking...";
+
+    const robotMove = moveFromRobot();
+
+    console.log(robotMove);
+
+    setTimeout(()=>{
+        
+        switch (checkTheWinner(robotMove, userMove)) {
+            case 'draw':
+                flyerModel('It Was A Draw.', 'warning');
+                break;
+            case '1':
+                flyerModel('Robot Won The Round.','failed');
+                break;
+            case '2':
+                flyerModel('You Won The Round.', 'success');
+                break;
+        }
+        // TODO: update score 
+        // TODO: update the timeline
+        // TODO: update the pics of the robot
+
+        // to avoid clash with flyer model
+        setTimeout(()=>{
+            selectedMove.innerHTML = "Choose Your Move...";
+            descriptionFromRobot.innerHTML = "Waiting For User Action...";
+            startListeningToUserEvents();
+        },1500);
+
+    },1000);
+}
+
+
+// event binding helper functions
+function functionForRock(){
+    userChooseMove('Rock');
+}
+
+function functionForPaper(){
+    userChooseMove('Paper');
+}
+
+function functionForScissor(){
+    userChooseMove('Scissor');
+}
+
+//add eventlistener to buttons
+function startListeningToUserEvents(){
+
+    moveCollections.classList.remove("optionDisabled");
+    moveCollections.classList.add("userOptions");
+
+    //event binder
+    rockBtn.addEventListener("click",functionForRock);
+    paperBtn.addEventListener("click",functionForPaper);
+    scissorBtn.addEventListener("click",functionForScissor);
+}
+
 //updates the user front end 
 function updateDisplayUserName(){
     let user = firebase.auth().currentUser;
@@ -129,6 +235,7 @@ firebase.auth().onAuthStateChanged((user)=>{
             element.classList.remove("pageLoading");
         });
 
+        startListeningToUserEvents();
         actionPanelClose();
         updateDisplayUserName();
     }else{
